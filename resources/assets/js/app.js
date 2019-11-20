a9d98bfb87262af21bb7bf129aabd839f1372946
Douglas Márquez <douggsv@users.noreply.github.com>
2019-11-20 19:30:26 -0400

    Proyecto

---
+++ resources/assets/js/app.js
@@ -0,0 +1,13 @@
+//require('./bootstrap');
+
+window.Vue = require('vue');
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+const app = new Vue({
+    el: '#app',
+    data:{
+        menu:0
+    }
+
+});
---
+++ resources/views/contenido/contenido.blade.php
@@ -0,0 +1,61 @@
+@extends('principal')
+@section('contenido')
+
+    <!-- CONTENIDO DE CADA MENÚ. -->
+
+<template v-if="menu==0">
+<example-component></example-component>
+</template>
+
+<template v-if="menu==1">
+<h1>Mostrando Mostrando contenido menu 1</h1>
+</template>
+
+<template v-if="menu==2">
+<h1>Contenido menu 2</h1>
+</template>
+
+<template v-if="menu==3">
+<h1>Contenido menu 3</h1>
+</template>
+
+<template v-if="menu==4">
+<h1>Contenido menu 4</h1>
+</template>
+
+<template v-if="menu==5">
+<h1>Contenido menu 5</h1>
+</template>
+
+<template v-if="menu==6">
+<h1>Contenido menu 6</h1>
+</template>
+
+<template v-if="menu==7">
+<h1>Contenido menu 7</h1>
+</template>
+
+<template v-if="menu==8">
+<h1>Contenido menu 8</h1>
+</template>
+
+<template v-if="menu==9">
+<h1>Contenido menu 9</h1>
+</template>
+
+<template v-if="menu==10">
+<h1>Contenido menu 10</h1>
+</template>
+
+<template v-if="menu==11">
+<h1>Contenido menu 11</h1>
+</template>
+
+<template v-if="menu==12">
+<h1>Contenido menu 12</h1>
+</template>
+
+
+
+
+        @endsection
